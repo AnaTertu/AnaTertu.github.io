149a2331d83771ab5166b778027faf64cfd0a5e7
AnaTertu <ana.tertu.2019@gmail.com>
2021-07-19 11:33:46 -0300

    JavaScript in section list - Hard Skills

--- scripts/content.js
+++ scripts/content.js
@@ -9,14 +9,42 @@ function professional() {
 
     const args = { 
         firstColumn: [
-            { text: "HTML", valuenow: 100},
-            { text: "CSS", valuenow: 85},
-            { text: "JavaScript", valuenow: 75},
+            { text: "GIT", valuenow: 30},
+            { text: "Algorithm", valuenow: 25},
+            { text: "Object Orientation", valuenow: 25},
         ],
         secondColumn: [
-            { text: "Adobe Photoshop", valuenow: 75},
-            { text: "Sketch", valuenow: 85},
-            { text: "Adobe XD", valuenow: 80},
+            { text: "Logic programming", valuenow: 25},
+            { text: "Word", valuenow: 50},
+            { text: "Excell", valuenow: 35},
+        ],
+        thirdColumn: [
+            { text: "HTML", valuenow: 35},
+            { text: "CSS", valuenow: 25},
+            { text: "JavaScript", valuenow: 20},
+        ],
+        fourthColumn: [
+            { text: "Bootstrap", valuenow: 20},
+            { text: "React", valuenow: 20},
+            { text: "React Native", valuenow: 15},
+        ],
+        fifthColumn: [
+            { text: "Php", valuenow: 10},
+            { text: "NodeJs", valuenow: 10},
+            { text: "MySQL", valuenow: 15},
+        ],
+        sixthColumn: [
+            { text: "PhpMyAdmin", valuenow: 15},
+            { text: "NoSQL (MongoDB)", valuenow: 85},
+            { text: "React", valuenow: 10},
+        ],
+        seventhColumn: [
+            { text: "Linux", valuenow: 30},
+            { text: "Windows", valuenow: 50},
+        ],
+        eightColumn: [
+            { text: "English", valuenow: 20},
+            { text: "Español", valuenow: 20},
         ]
     }
 
@@ -25,12 +53,36 @@ function professional() {
 
 function progressBody(args) {
 
-    document.getElementById('contentId').appendChild(
+    document.getElementById('hardId').appendChild(
         progressColumn(args.firstColumn, "bg-primary", 0)
     );
 
-    document.getElementById('contentId').appendChild(
+    document.getElementById('hardId').appendChild(
         progressColumn(args.secondColumn, "bg-success", args.firstColumn.length)
+    );
+
+    document.getElementById('frontId').appendChild(
+        progressColumn(args.thirdColumn, "bg-primary", 0)
+    );
+
+    document.getElementById('frontId').appendChild(
+        progressColumn(args.fourthColumn, "bg-success", args.thirdColumn.length)
+    );
+
+    document.getElementById('backId').appendChild(
+        progressColumn(args.fifthColumn, "bg-primary", 0)
+    );
+
+    document.getElementById('backId').appendChild(
+        progressColumn(args.sixthColumn, "bg-success", args.fifthColumn.length)
+    );
+
+    document.getElementById('sistLangId').appendChild(
+        progressColumn(args.seventhColumn, "bg-primary", 0)
+    );
+
+    document.getElementById('sistLangId').appendChild(
+        progressColumn(args.eightColumn, "bg-success", args.seventhColumn.length)
     );
 }
 
